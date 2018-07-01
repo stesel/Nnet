e703302ee7d66ef378c114b7603da7d83034d5e8
Leo <stesel23@gmail.com>
2018-07-01 12:38:57 +0300

    NN-00 Add rx/draw svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
 import * as React from "react";
+import * as Rx from "rx";
+import * as Ramda from "ramda";
+
+type Point = Readonly<{
+  x: number;
+  y: number;
+}>;
+
+const MAX_X = 400;
+const MAX_Y = 400;
 
 const appStyle: React.CSSProperties = {
   "text-align": "center"
@@ -14,8 +24,12 @@ const appHeaderStyle: React.CSSProperties = {
 
 const contentStyle: React.CSSProperties = {
   border: "2px solid black",
-  padding: "8px"
+  padding: "8px",
+  margin: "3px"
 };
+
+const random = (high: number, low: number) =>
+  Math.random() * (high - low) + low;
 
 export class App extends React.PureComponent {
   public render() {
@@ -27,7 +41,27 @@ export class App extends React.PureComponent {
     );
   }
 
+  private getSVG = (randomPoints: Point[]) => (
+    <svg width={MAX_X} height={MAX_Y}>
+      <rect
+        width="100%"
+        height="100%"
+        stroke="red"
+        strokeWidth="2"
+        fill="none"
+      />
+      {randomPoints.map(point => <circle cx={point.x} cy={point.y} r="3" />)}
+      <line x1="0" x2={MAX_X} y1="0" y2={MAX_Y} stroke="purple" />
+    </svg>
+  );
+
   private content = () => {
-    return <div style={contentStyle}>CONTENT</div>;
+    let randomPoints: Point[];
+    randomPoints = Ramda.range(0, 100).map((n: number) => ({
+      x: random(0, MAX_X),
+      y: random(0, MAX_Y)
+    }));
+    Rx.Observable.from(randomPoints).subscribe(p => console.log(p));
+    return <div style={contentStyle}>{this.getSVG(randomPoints)}</div>;
   };
 }
