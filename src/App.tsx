abafcbd9cf2d7d3222bf788db34d4e1861d0f6a2
Leo <stesel23@gmail.com>
2018-07-02 20:03:58 +0300

    NN-00 Add simple guess logics

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,21 @@ type Point = Readonly<{
   y: number;
 }>;
 
+type Team = -1 | 1;
+
 const MAX_X = 400;
 const MAX_Y = 400;
 
 const appStyle: React.CSSProperties = {
-  "text-align": "center"
+  textAlign: "center"
 };
 
 const appHeaderStyle: React.CSSProperties = {
-  "background-color": "#222",
+  backgroundColor: "#222",
   height: "100%",
   padding: "20px",
   color: "white",
-  "margin-bottom": "20px"
+  marginBottom: "20px"
 };
 
 const contentStyle: React.CSSProperties = {
@@ -31,6 +33,24 @@ const contentStyle: React.CSSProperties = {
 const random = (high: number, low: number) =>
   Math.random() * (high - low) + low;
 
+const guess = (weights: Point, point: Point) => {
+  const sum = point.x * weights.x + point.y * weights.y;
+  const team = sum >= 0 ? 1 : -1;
+  return team;
+};
+
+const team = (point: Point) => (point.x > point.y ? 1 : -1);
+
+const train = (weights: Point, point: Point, team: Team) => {
+  const guessResult = guess(weights, point); // 1
+  const error = team - guessResult;
+  const correlation = {
+    x: weights.x + point.x * error,
+    y: weights.y + point.y * error
+  };
+  return correlation;
+};
+
 export class App extends React.PureComponent {
   public render() {
     return (
@@ -41,7 +61,7 @@ export class App extends React.PureComponent {
     );
   }
 
-  private getSVG = (randomPoints: Point[]) => (
+  private getSVG = (randomWeights: Point, randomPoints: Point[]) => (
     <svg width={MAX_X} height={MAX_Y}>
       <rect
         width="100%"
@@ -50,18 +70,45 @@ export class App extends React.PureComponent {
         strokeWidth="2"
         fill="none"
       />
-      {randomPoints.map(point => <circle cx={point.x} cy={point.y} r="3" />)}
+      {randomPoints.map((point, index) => (
+        <circle
+          key={`point_${index}`}
+          cx={point.x}
+          cy={point.y}
+          r="3"
+          fill={guess(randomWeights, point) > 0 ? "red" : "blue"}
+        />
+      ))}
       <line x1="0" x2={MAX_X} y1="0" y2={MAX_Y} stroke="purple" />
     </svg>
   );
 
   private content = () => {
-    let randomPoints: Point[];
-    randomPoints = Ramda.range(0, 100).map((n: number) => ({
+    const randomPoints = Ramda.range(0, 200).map((n: number) => ({
       x: random(0, MAX_X),
       y: random(0, MAX_Y)
     }));
-    Rx.Observable.from(randomPoints).subscribe(p => console.log(p));
-    return <div style={contentStyle}>{this.getSVG(randomPoints)}</div>;
+
+    const randomWeights = {
+      x: random(-1, 1),
+      y: random(-1, 1)
+    };
+
+    const trainedWeights = [
+      { x: 721, y: 432 },
+      { x: 211, y: 122 },
+      { x: 328, y: 833 },
+      { x: 900, y: 400 }
+    ].reduce((w: Point, p: Point) => {
+      return train(w, p, team(p));
+    }, randomWeights);
+
+    Rx.Observable.of(trainedWeights).subscribe((p: Point) => console.log(p));
+
+    return (
+      <div style={contentStyle}>
+        {this.getSVG(trainedWeights, randomPoints)}
+      </div>
+    );
   };
 }
